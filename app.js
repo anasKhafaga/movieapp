1e96296ddc940e58dd5e855891d4b43231c5eb3e
Anas Khafaga <khafaga1998@gmail.com>
2020-04-28 17:10:49 +0200

    intro to res object

--- app.js
+++ app.js
@@ -2,4 +2,15 @@ const express = require('express');
 
 const app = express();
 
+// routes
+app.get('/', (req, res, next) => {
+  res.redirect('/user');
+});
+ 
+app.get('/user', (req, res, next) => {
+  res.send('Welcome to user page');
+})
+
+
+
 module.exports = app;
