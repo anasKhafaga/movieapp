0b228e99718d5a827507257bce6c679fbf0e9d98
Anas Khafaga <khafaga1998@gmail.com>
2020-04-28 17:27:50 +0200

    intro to req object

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const app = express();
 
 // routes
 app.get('/', (req, res, next) => {
-  res.redirect('/user');
+  res.send('Welcome to homepage');
 });
  
-app.get('/user', (req, res, next) => {
+app.get('/user/:id/:postId', (req, res, next) => {
+  const host = req.get('Host');
+  console.log(host);
   res.send('Welcome to user page after nodemon');
 })
 
