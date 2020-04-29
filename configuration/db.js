7867a8266597d10bcc9bd3a00d09ee682f662bfb
Anas Khafaga <khafaga1998@gmail.com>
2020-04-29 05:45:07 +0200

    db config and testing[2videos]

---
+++ configuration/db.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require('mongodb');
+
+const _uri =
+  'mongodb+srv://anasSaber:anasSaber1234@firstproject-05irj.mongodb.net/sample_mflix?retryWrites=true&w=majority';
+
+const dbCon = (coll, cb) => {
+  MongoClient.connect(_uri)
+    .then(async (client) => { 
+      const db = client.db('sample_mflix').collection(coll);
+      await cb(db);
+      client.close();
+    })
+    .catch();
+};
+
+dbCon('movies', async (db) => {
+  const movie = await db.findOne();
+  console.log(movie);
+})
+ 
+module.exports = dbCon;
