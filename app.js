3f63c1772c959bb40872d1174d8de64a0bf995a9
Anas Khafaga <khafaga1998@gmail.com>
2020-04-29 17:11:58 +0200

    not found error

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { logger } = require('./configuration')
+const createError = require('http-errors');
 
 const middleware = require('./middlewares');
 
@@ -17,6 +18,9 @@ middleware(app);
 // routes
 routes(app);
 
-
+app.use((req, res, next) => {
+  const error = createError(404);
+  res.status(error.statusCode).send(error.message);
+ });
 
 module.exports = app;
