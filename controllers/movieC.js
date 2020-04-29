5dbd96f509c7eeb6bd89e5966b8ccfe5b2e927f7
Anas Khafaga <khafaga1998@gmail.com>
2020-04-29 14:00:44 +0200

    finish movies route and pagination

--- controllers/movieC.js
+++ controllers/movieC.js
@@ -1,5 +1,18 @@
+const { dbCon } = require('../configuration')
+
 const getMovies = (req, res, next) => { 
-  res.send('wait for movies');
+  const pageNum = parseInt(req.params.page);
+
+  if (isNaN(pageNum)) {
+    return res.status(400).send('bad request');
+  }
+
+  const moviesToSkip = (pageNum - 1) * 10;
+
+  dbCon('movies', async (db) => { 
+    const movies = await db.find({}).skip(moviesToSkip).limit(10).toArray();
+    res.json(movies);
+  });
 };
 
 module.exports = {
