f8ba3bd64f9b3af0fa9275b90e25b92489e0331e
Anas Khafaga <khafaga1998@gmail.com>
2020-04-30 08:19:23 +0200

    check existence in post singup route

--- controllers/auth/signup.js
+++ controllers/auth/signup.js
@@ -1,12 +1,26 @@
 const { User } = require('../../models')
+const createError = require('http-errors');
 
 const postSignup = (req, res, next) => { 
+  // validation
   const validation = User.validate(req.body);
   if (validation.error) {
     const error = new Error(validation.error.message);
     error.statusCode = 400;
     return next(error);
   }
+
+  // check existence
+  const user = new User(req.body);
+  user.checkExistence()
+    .then(result => {
+      if (result.check) {
+        const error = new Error(result.message);
+        error.statusCode = 409;
+        return next(error);
+      }
+    })
+    .catch(err => next(createError(500)));
 };
 
 module.exports = {postSignup};
