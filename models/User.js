82a049fea4a79399b7ce8779eca5ed1cbe089e0d
Anas Khafaga <khafaga1998@gmail.com>
2020-04-30 09:07:01 +0200

    password hashing

--- models/User.js
+++ models/User.js
@@ -1,14 +1,17 @@
-const { dbCon } = require('../configuration')
+const { dbCon } = require('../configuration');
 const { userValidator } = require('../validator');
+const { hashSync } = require('bcryptjs');
 
-class User { 
-  constructor(userData) { 
+class User {
+  constructor(userData) {
     this.userData = { ...userData };
-  };
+  }
 
-  save(cb) { 
-    dbCon('users', async (db) => { 
+  save(cb) {
+    dbCon('users', async (db) => {
       try {
+        const hashedPass = hashSync(this.userData['password'], 12);
+        this.userData['password'] = hashedPass;
         await db.insertOne(this.userData);
         cb();
       } catch (err) {
@@ -17,41 +20,42 @@ class User {
     });
   }
 
-  checkExistence() { 
-    return new Promise((resolve, reject) => { 
-
+  checkExistence() {
+    return new Promise((resolve, reject) => {
       dbCon('users', async (db) => {
         try {
-          const user = await db.findOne({ '$or': [{ username: this.userData['username'] }, { email: this.userData['email'] }] });
+          const user = await db.findOne({
+            $or: [
+              { username: this.userData['username'] },
+              { email: this.userData['email'] },
+            ],
+          });
 
           if (!user) {
             resolve({
-              check: false
-            })
+              check: false,
+            });
           } else if (this.userData['username'] === user.username) {
             resolve({
               check: true,
-              message: 'this username is already in use'
-            })
+              message: 'this username is already in use',
+            });
           } else if (this.userData['email'] === user.email) {
             resolve({
               check: true,
-              message: 'this eamil is already in use'
-            })
+              message: 'this eamil is already in use',
+            });
           }
         } catch (err) {
           reject(err);
         }
       });
-
     });
-  };
+  }
 
   static validate(userData) {
     return userValidator.validate(userData);
-  };
-  
-};
-
+  }
+}
 
 module.exports = User;
