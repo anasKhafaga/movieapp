81c635085414d775797fad9a6554198a90e2700f
Anas Khafaga <khafaga1998@gmail.com>
2020-05-01 11:32:44 +0200

    fixing a bug with db config file

--- configuration/db.js
+++ configuration/db.js
@@ -6,7 +6,10 @@ const dbCon = (coll, cb, coll2) => {
   MongoClient.connect(_uri)
     .then(async (client) => {
       const db = client.db('sample_mflix').collection(coll);
-      const db2 = client.db('sample_mflix').collection(coll2);
+      let db2;
+      if (coll2) {
+        db2 = client.db('sample_mflix').collection(coll2);
+      }
       await cb(db, db2);
       client.close();
     });
