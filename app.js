a573f06e5a5ea2a3fd02931f4d15cf0627efc76e
Anas Khafaga <khafaga1998@gmail.com>
2020-05-02 06:40:06 +0200

    app.js and index file documentation

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+/**
+ * Express app
+ * @module app
+ * @requires module:configuration
+ * @requires module:middlewares
+ * @requires module:routes
+ */
+
 const express = require('express');
 const { logger } = require('./configuration')
 const createError = require('http-errors');
@@ -6,6 +14,11 @@ const {middleware} = require('./middlewares');
 
 const routes = require('./routes')
 
+/**
+ * @type {Object}
+ * @namespace app
+ * @const
+ */
 const app = express();
 
 process.on('unhandledRejection', (reason) => {
@@ -13,9 +26,16 @@ process.on('unhandledRejection', (reason) => {
   process.exit(1);
 });
 
+/**
+ * Executes middlewares in app.js file
+ * @function middleware {@link module:middleware}
+ */
 middleware(app);
 
-// routes
+/**
+ * Executes routes in app.js file
+ * @function routes {@link module:routes}
+ */
 routes(app);
 
 app.use((req, res, next) => {
@@ -23,7 +43,20 @@ app.use((req, res, next) => {
   next(error);
 });
  
-app.use((error, req, res, next) => { 
+/**
+ * @function use
+ * @param {Callback} errorHandler - global handler
+ */
+app.use(
+  /**
+   * returns a user friendly response for errors
+   * @function errorHandler
+   * @param {Object} error
+   * @param {Object} req
+   * @param {Object} res
+   * @param {Callback} next
+   */
+  (error, req, res, next) => { 
   logger.error(error.message);
 
   res.statusCode = error.statusCode;
--- index.js
+++ index.js
@@ -2,8 +2,17 @@ require('dotenv').config();
 const http = require('http');
 const app = require('./app');
 
+/**
+ * @type {httpServer}
+ * @const
+ */
 const server = http.createServer(app);
 
+/**
+ * @function serverListener
+ * @param {number} port - port
+ * @param {Callback} listener
+ */
 server.listen(process.env.PORT, () => {
   console.log(`Server is listening now on port ${process.env.PORT}`);
 });
